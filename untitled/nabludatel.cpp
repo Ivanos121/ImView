f0b5422596ca40f88336703425474e08d6ed5d47
ivanos121 <loggie121@gmail.com>
2020-11-20 15:23:50 +0300

    polusovka

--- untitled/nabludatel.cpp
+++ untitled/nabludatel.cpp
@@ -143,16 +143,16 @@ void Nabludatel::rasch(DataSourceBVAS *dataSourceBVAS)
     Uc = dataSourceBVAS->Uc[i];
 
     iaizm=sqrt(2.0/3.0)*(Ia-(Ib+Ic)/2.0);
-    iaizm = iaCorrector.apply(iaizm,Kint,1.0,0.3,Ts);
+    iaizm = iaCorrector.apply(iaizm,Kint,1.0,2.0,8.0,Ts);
 
     ibizm=(1/sqrt(2.0)) * (Ib - Ic);
-    ibizm = ibCorrector.apply(ibizm,Kint,1.0,0.3,Ts);
+    ibizm = ibCorrector.apply(ibizm,Kint,1.0,2.0,8.0,Ts);
 
     uaizm=sqrt(2.0/3.0)*(Ua-(Ub+Uc)/2.0);
-    uaizm = uaCorrector.apply(uaizm,Kint,1.0,0.3,Ts);
+    uaizm = uaCorrector.apply(uaizm,Kint,1.0,2.0,8.0,Ts);
 
     ubizm=(1/sqrt(2.0)) * (Ub - Uc);
-    ubizm = ubCorrector.apply(ubizm,Kint,1.0,0.3,Ts);
+    ubizm = ubCorrector.apply(ubizm,Kint,1.0,2.0,8.0,Ts);
 
     a3=-pn*beta*w_prev;
     a6=pn*w_prev;
@@ -251,6 +251,10 @@ void Nabludatel::rasch(DataSourceBVAS *dataSourceBVAS)
 
         L2 = Lm + Lk - (L1 - Lm);
     }
+    // ВРЕМЕНННО!!!
+
+    L2 = Lk0 + Lm - (L1 - Lm);
+    R2 = 2.4;
 
     printf("R2 = %g L2 = %g s=%g\n", R2, L2,s);
 
@@ -301,11 +305,11 @@ void Nabludatel::rasch(DataSourceBVAS *dataSourceBVAS)
     cos_f=p_akt/p_poln;
 }
 
-double ZeroCorrector::apply(double z, double K, double K1, double K2, double Ts)
+double ZeroCorrector::apply(double z, double K, double K1, double K2, double K3, double Ts)
 {
     double z_int = 1.0/2.0*(2*z_int_prev + (z - y_prev)*K*Ts + (z_prev - y_prev)*K*Ts);
     double u = (1.0/(2+K*Ts)) * (u_prev * (2-K1*K*Ts)+z_int*K*Ts+z_int_prev*K*Ts);
-    double y = 1.0/2.0*(2*y_prev + 2*u - 2*u_prev + u*K2*K*Ts + u_prev*K2*K*Ts);
+    double y = 1.0/2.0*(2*y_prev + 2*u*K3 - 2*u_prev*K3 + u*K2*K*Ts + u_prev*K2*K*Ts);
     u_prev = u;
     y_prev = y;
     z_prev = z;
--- untitled/nabludatel.h
+++ untitled/nabludatel.h
@@ -7,7 +7,7 @@ class ZeroCorrector
 {
 public:
     double u_prev=0, z_prev=0, z_int_prev=0, y_prev=0;
-    double apply(double z, double K, double K1, double K2, double Ts);
+    double apply(double z, double K, double K1, double K2, double K3, double Ts);
 };
 
 class Nabludatel
